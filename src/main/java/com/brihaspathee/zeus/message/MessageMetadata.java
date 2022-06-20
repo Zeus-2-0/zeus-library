a35f01920acd9e88545b8589ff75206acc064639
vbalaji215-dev <vbalaji215@outlook.com>
2022-06-20 08:48:34 -0400

    Added message payload template

---
+++ src/main/java/com/brihaspathee/zeus/message/MessageMetadata.java
@@ -0,0 +1,35 @@
+package com.brihaspathee.zeus.message;
+
+import lombok.*;
+
+/**
+ * Created in Intellij IDEA
+ * User: Balaji Varadharajan
+ * Date: 20, June 2022
+ * Time: 8:45 AM
+ * Project: Zeus
+ * Package Name: com.brihaspathee.zeus.message
+ * To change this template use File | Settings | File and Code Template
+ */
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageMetadata {
+
+    private String messageSource;
+
+    private String messageDestination;
+
+    private String messageCreationTimestamp;
+
+    @Override
+    public String toString() {
+        return "MessageMetadata{" +
+                "messageSource='" + messageSource + '\'' +
+                ", messageDestination='" + messageDestination + '\'' +
+                ", messageCreationTimestamp='" + messageCreationTimestamp + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/brihaspathee/zeus/message/ZeusMessagePayload.java
@@ -0,0 +1,25 @@
+package com.brihaspathee.zeus.message;
+
+import lombok.*;
+
+/**
+ * Created in Intellij IDEA
+ * User: Balaji Varadharajan
+ * Date: 20, June 2022
+ * Time: 8:45 AM
+ * Project: Zeus
+ * Package Name: com.brihaspathee.zeus.message
+ * To change this template use File | Settings | File and Code Template
+ */
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ZeusMessagePayload<T> {
+
+    private MessageMetadata messageMetadata;
+
+    private T payload;
+
+}
