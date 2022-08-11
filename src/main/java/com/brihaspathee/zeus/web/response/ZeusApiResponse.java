6d34e22143319376b1a77c25ac3e26de18c0792c
vbalaji215-dev <vbalaji215@outlook.com>
2022-08-11 12:55:01 -0400

    added timestamp in zeus api response

--- src/main/java/com/brihaspathee/zeus/web/response/ZeusApiResponse.java
+++ src/main/java/com/brihaspathee/zeus/web/response/ZeusApiResponse.java
@@ -30,6 +30,7 @@ public class ZeusApiResponse<E> {
      * Time stamp when the response was generated
      */
     @JsonFormat(pattern="dd MMM yyyy hh:mm:ss")
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
     private LocalDateTime timestamp;
 
     private int statusCode;
