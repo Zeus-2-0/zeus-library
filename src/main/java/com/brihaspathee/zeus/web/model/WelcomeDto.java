3de2d717fc50bdebdb08e24bb33f03546f2aa185
vbalaji215-dev <vbalaji215@outlook.com>
2022-09-24 08:04:21 -0400

    Added Welcome dto

---
+++ src/main/java/com/brihaspathee/zeus/web/model/WelcomeDto.java
@@ -0,0 +1,25 @@
+package com.brihaspathee.zeus.web.model;
+
+import lombok.*;
+
+/**
+ * Created in Intellij IDEA
+ * User: Balaji Varadharajan
+ * Date: 24, September 2022
+ * Time: 8:03 AM
+ * Project: Zeus
+ * Package Name: com.brihaspathee.zeus.web.model
+ * To change this template use File | Settings | File and Code Template
+ */
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class WelcomeDto {
+
+    /**
+     * A short welcome message for the service
+     */
+    private String welcomeMessage;
+}
