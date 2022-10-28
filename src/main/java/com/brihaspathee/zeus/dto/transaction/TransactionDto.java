fc2c0007fdfe76a5ef4630fbf275ab2d6dbf234d
vbalaji215-dev <vbalaji215@outlook.com>
2022-10-28 17:39:07 -0400

    Update date format

--- src/main/java/com/brihaspathee/zeus/dto/transaction/TransactionDto.java
+++ src/main/java/com/brihaspathee/zeus/dto/transaction/TransactionDto.java
@@ -56,7 +56,7 @@ public class TransactionDto {
      * The date and time when the transaction was received
      */
     @JsonProperty(required = false)
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern ="yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", timezone="EST" )
     @Schema(description = "The date when the transaction was received", example = "12/15/2021", required = false, accessMode = Schema.AccessMode.READ_ONLY)
     private LocalDateTime transactionReceivedDate;
 
