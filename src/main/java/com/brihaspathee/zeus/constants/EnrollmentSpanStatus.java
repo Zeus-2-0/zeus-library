710fd053dca356b9b294683b0e304c7d1c145730
vbalaji215-dev <vbalaji215@outlook.com>
2022-11-24 07:32:03 -0500

    Added enrollment span status

---
+++ src/main/java/com/brihaspathee/zeus/constants/EnrollmentSpanStatus.java
@@ -0,0 +1,15 @@
+package com.brihaspathee.zeus.constants;
+
+/**
+ * Created in Intellij IDEA
+ * User: Balaji Varadharajan
+ * Date: 24, November 2022
+ * Time: 7:30 AM
+ * Project: Zeus
+ * Package Name: com.brihaspathee.zeus.constants
+ * To change this template use File | Settings | File and Code Template
+ */
+public enum EnrollmentSpanStatus {
+
+    ENROLL, PREMEM, NO_VALID_STATUS, CANCEL, SUSPEND, DELINQ, ELECT
+}
