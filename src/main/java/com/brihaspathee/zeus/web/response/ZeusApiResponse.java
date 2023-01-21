ac4572f6f278ea724e48e27c0b0847d10f90f6c2
vbalaji215-dev <vbalaji215@outlook.com>
2023-01-21 15:32:57 -0500

    Updated local date time format

--- src/main/java/com/brihaspathee/zeus/web/response/ZeusApiResponse.java
+++ src/main/java/com/brihaspathee/zeus/web/response/ZeusApiResponse.java
@@ -32,7 +32,6 @@ public class ZeusApiResponse<E> {
 //    @JsonFormat(pattern="dd MMM yyyy hh:mm:ss")
 //    @JsonSerialize(using = LocalDateTimeSerializer.class)
 //    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
-    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern ="yyyy-MM-dd'T'HH:mm:ss.SSSZ", timezone="EST" )
     private LocalDateTime timestamp;
 
     private int statusCode;
