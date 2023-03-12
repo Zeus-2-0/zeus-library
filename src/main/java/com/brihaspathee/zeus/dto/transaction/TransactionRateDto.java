9389081cab4937d050dc24d049efc06281f694ff
vbalaji215-dev <vbalaji215@outlook.com>
2023-03-12 16:00:55 -0400

    Added CSR Variant to transaction rate dto

--- src/main/java/com/brihaspathee/zeus/dto/transaction/TransactionRateDto.java
+++ src/main/java/com/brihaspathee/zeus/dto/transaction/TransactionRateDto.java
@@ -36,7 +36,7 @@ public class TransactionRateDto {
      * The primary key of the transaction rate
      */
     @JsonProperty(required = false)
-    @Schema(description = "Transaction Rate SK - This is a UUID value", example = "657cfd75-634e-49f1-9556-4d79f79848ec", required = false, accessMode = Schema.AccessMode.READ_ONLY)
+    @Schema(description = "Transaction Rate SK - This is a UUID value", example = "657cfd75-634e-49f1-9556-4d79f79848ec", requiredMode = Schema.RequiredMode.NOT_REQUIRED, accessMode = Schema.AccessMode.READ_ONLY)
     @Null
     private UUID transactionRateSK;
 
@@ -50,22 +50,26 @@ public class TransactionRateDto {
      * The rate type - E.g. PREAMTOT, APTCAMT etc.
      */
     @JsonProperty(required = true)
-    @Schema(description = "The rate type", example = "PREAMTOT", required = true, accessMode = Schema.AccessMode.READ_WRITE)
+    @Schema(description = "The rate type", example = "PREAMTOT", requiredMode = Schema.RequiredMode.REQUIRED, accessMode = Schema.AccessMode.READ_WRITE)
     private String rateTypeCode;
 
     /**
      * The value that was received for the rate type
      */
     @JsonProperty(required = true)
-    @Schema(description = "The value that was received for the rate type", example = "155.65", required = true, accessMode = Schema.AccessMode.READ_WRITE)
+    @Schema(description = "The value that was received for the rate type", example = "155.65", requiredMode = Schema.RequiredMode.REQUIRED, accessMode = Schema.AccessMode.READ_WRITE)
     private BigDecimal transactionRate;
+
+    @JsonProperty(required = true)
+    @Schema(description = "The CSR Variant that is associated with the rate", example = "01", requiredMode = Schema.RequiredMode.REQUIRED, accessMode = Schema.AccessMode.READ_WRITE)
+    private String csrVariant;
 
     /**
      * The start date of the rate
      */
     @JsonProperty(required = true)
     @JsonFormat(pattern="yyyy-MM-dd")
-    @Schema(description = "The start date of the rate", example = "6/1/2021", required = false, accessMode = Schema.AccessMode.READ_WRITE)
+    @Schema(description = "The start date of the rate", example = "6/1/2021", requiredMode = Schema.RequiredMode.REQUIRED, accessMode = Schema.AccessMode.READ_WRITE)
     private LocalDate rateStartDate;
 
     /**
@@ -73,21 +77,21 @@ public class TransactionRateDto {
      */
     @JsonProperty(required = false)
     @JsonFormat(pattern="yyyy-MM-dd")
-    @Schema(description = "The end date of the rate", example = "8/31/2021", required = false, accessMode = Schema.AccessMode.READ_WRITE)
+    @Schema(description = "The end date of the rate", example = "8/31/2021", requiredMode = Schema.RequiredMode.NOT_REQUIRED, accessMode = Schema.AccessMode.READ_WRITE)
     private LocalDate rateEndDate;
 
     /**
      * The date when the record was created
      */
     @JsonProperty(required = false)
-    @Schema(description = "The date when the record was created", example = "12/15/2021", required = false, accessMode = Schema.AccessMode.READ_WRITE)
+    @Schema(description = "The date when the record was created", example = "12/15/2021", requiredMode = Schema.RequiredMode.NOT_REQUIRED, accessMode = Schema.AccessMode.READ_WRITE)
     private LocalDateTime createdDate;
 
     /**
      * The date when the record was updated
      */
     @JsonProperty(required = false)
-    @Schema(description = "The date when the record was updated", example = "12/15/2021", required = false, accessMode = Schema.AccessMode.READ_WRITE)
+    @Schema(description = "The date when the record was updated", example = "12/15/2021", requiredMode = Schema.RequiredMode.NOT_REQUIRED, accessMode = Schema.AccessMode.READ_WRITE)
     private LocalDateTime updatedDate;
 
     /**
