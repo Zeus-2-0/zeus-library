7dc85e90bb39220bc79498b118527510aded50b4
vbalaji215-dev <vbalaji215@outlook.com>
2023-04-14 10:38:39 -0400

    Added premium status type code

--- src/main/java/com/brihaspathee/zeus/dto/account/PremiumSpanDto.java
+++ src/main/java/com/brihaspathee/zeus/dto/account/PremiumSpanDto.java
@@ -40,7 +40,7 @@ public class PremiumSpanDto {
      * The primary key of the premium span
      */
     @JsonProperty(required = false)
-    @Schema(description = "Premium Span SK - This is a UUID value", example = "657cfd75-634e-49f1-9556-4d79f79848ec", required = false, accessMode = Schema.AccessMode.READ_ONLY)
+    @Schema(description = "Premium Span SK - This is a UUID value", example = "657cfd75-634e-49f1-9556-4d79f79848ec", requiredMode = Schema.RequiredMode.NOT_REQUIRED, accessMode = Schema.AccessMode.READ_ONLY)
     @Null
     private UUID premiumSpanSK;
 
@@ -48,21 +48,21 @@ public class PremiumSpanDto {
      * The enrollment span that the premium span is associated
      */
     @JsonProperty(required = false)
-    @Schema(description = "Enrollment Span SK - This is a UUID value", example = "657cfd75-634e-49f1-9556-4d79f79848ec", required = false, accessMode = Schema.AccessMode.READ_ONLY)
+    @Schema(description = "Enrollment Span SK - This is a UUID value", example = "657cfd75-634e-49f1-9556-4d79f79848ec", requiredMode = Schema.RequiredMode.NOT_REQUIRED, accessMode = Schema.AccessMode.READ_ONLY)
     private UUID enrollmentSpanSK;
 
     /**
      * The zeus transaction control number of the transaction that created the premium span
      */
     @JsonProperty(required = true)
-    @Schema(description = "The zeus transaction control number of the transaction that created the premium span", example = "SDFG43456DFG23G", required = true, accessMode = Schema.AccessMode.READ_WRITE)
+    @Schema(description = "The zeus transaction control number of the transaction that created the premium span", example = "SDFG43456DFG23G", requiredMode = Schema.RequiredMode.REQUIRED, accessMode = Schema.AccessMode.READ_WRITE)
     private String ztcn;
 
     /**
      * The unique code of the premium span in MMS
      */
     @JsonProperty(required = true)
-    @Schema(description = "Premium Span Code - Unique id that is assigned to the premium span in MMS", example = "DFEV323455DE5S3", required = true, accessMode = Schema.AccessMode.READ_WRITE)
+    @Schema(description = "Premium Span Code - Unique id that is assigned to the premium span in MMS", example = "DFEV323455DE5S3", requiredMode = Schema.RequiredMode.REQUIRED, accessMode = Schema.AccessMode.READ_WRITE)
     private String premiumSpanCode;
 
     /**
@@ -78,7 +78,7 @@ public class PremiumSpanDto {
     @JsonFormat(pattern="yyyy-MM-dd")
     @JsonSerialize(using = LocalDateSerializer.class)
     @JsonDeserialize(using = LocalDateDeserializer.class)
-    @Schema(description = "The start date of the premium span", example = "1/1/2021", required = true, accessMode = Schema.AccessMode.READ_WRITE)
+    @Schema(description = "The start date of the premium span", example = "1/1/2021", requiredMode = Schema.RequiredMode.REQUIRED, accessMode = Schema.AccessMode.READ_WRITE)
     private LocalDate startDate;
 
     /**
@@ -88,50 +88,58 @@ public class PremiumSpanDto {
     @JsonFormat(pattern="yyyy-MM-dd")
     @JsonSerialize(using = LocalDateSerializer.class)
     @JsonDeserialize(using = LocalDateDeserializer.class)
-    @Schema(description = "The end date of the premium span", example = "1/1/2021", required = true, accessMode = Schema.AccessMode.READ_WRITE)
+    @Schema(description = "The end date of the premium span", example = "1/1/2021", requiredMode = Schema.RequiredMode.REQUIRED, accessMode = Schema.AccessMode.READ_WRITE)
     private LocalDate endDate;
+
+    /**
+     * The status of the premium span
+     */
+    @JsonProperty(required = false)
+    @Schema(description = "Status of the premim span", example = "ACTIVE", requiredMode = Schema.RequiredMode.REQUIRED, accessMode = Schema.AccessMode.READ_WRITE)
+    private String statusTypeCode;
 
     /**
      * The CSR Variant associated with the premium span
      */
     @JsonProperty(required = true)
-    @Schema(description = "The CSR Variant of the premium span", example = "01", required = true, accessMode = Schema.AccessMode.READ_WRITE)
+    @Schema(description = "The CSR Variant of the premium span", example = "01", requiredMode = Schema.RequiredMode.REQUIRED, accessMode = Schema.AccessMode.READ_WRITE)
     private String csrVariant;
 
     /**
      * The total premium amount associated with the premium span
      */
     @JsonProperty(required = true)
-    @Schema(description = "The total premium amount period of the premium span", example = "150.00", required = true, accessMode = Schema.AccessMode.READ_WRITE)
+    @Schema(description = "The total premium amount period of the premium span", example = "150.00", requiredMode = Schema.RequiredMode.REQUIRED, accessMode = Schema.AccessMode.READ_WRITE)
     private BigDecimal totalPremiumAmount;
 
     /**
      * The total responsible amount associated with the premium span
      */
     @JsonProperty(required = true)
-    @Schema(description = "The total responsible amount period of the premium span", example = "150.00", required = true, accessMode = Schema.AccessMode.READ_WRITE)
+    @Schema(description = "The total responsible amount period of the premium span", example = "150.00", requiredMode = Schema.RequiredMode.REQUIRED, accessMode = Schema.AccessMode.READ_WRITE)
     private BigDecimal totalResponsibleAmount;
 
     /**
      * The APTC amount associated with the premium span
      */
     @JsonProperty(required = false)
-    @Schema(description = "The APTC amount period of the premium span", example = "150.00", required = false, accessMode = Schema.AccessMode.READ_WRITE)
+    @Schema(description = "The APTC amount period of the premium span", example = "150.00", requiredMode = Schema.RequiredMode.NOT_REQUIRED, accessMode = Schema.AccessMode.READ_WRITE)
     private BigDecimal aptcAmount;
 
     /**
      * The other pay amount associated with the premium span
      */
     @JsonProperty(required = false)
-    @Schema(description = "The other pay amount period of the premium span", example = "150.00", required = false, accessMode = Schema.AccessMode.READ_WRITE)
+    @Schema(description = "The other pay amount period of the premium span", example = "150.00", requiredMode = Schema.RequiredMode.NOT_REQUIRED, accessMode = Schema.AccessMode.READ_WRITE)
     private BigDecimal otherPayAmount;
 
     /**
      * The CSR Amount associated with the premium span
      */
     @JsonProperty(required = false)
-    @Schema(description = "The CSR amount period of the premium span", example = "150.00", required = false, accessMode = Schema.AccessMode.READ_WRITE)
+    @Schema(description = "The CSR amount period of the premium span", example = "150.00", requiredMode = Schema.RequiredMode.NOT_REQUIRED, accessMode = Schema.AccessMode.READ_WRITE)
     private BigDecimal csrAmount;
+
     /**
      * Identifies if the premium span was updated
      */
@@ -146,7 +154,7 @@ public class PremiumSpanDto {
     @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
     @JsonSerialize(using = LocalDateTimeSerializer.class)
     @JsonDeserialize(using = LocalDateTimeDeserializer.class)
-    @Schema(description = "The date when the premium span was created", example = "12/15/2021", required = false, accessMode = Schema.AccessMode.READ_WRITE)
+    @Schema(description = "The date when the premium span was created", example = "12/15/2021", requiredMode = Schema.RequiredMode.NOT_REQUIRED, accessMode = Schema.AccessMode.READ_WRITE)
     private LocalDateTime createdDate;
 
     /**
@@ -156,7 +164,7 @@ public class PremiumSpanDto {
     @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
     @JsonSerialize(using = LocalDateTimeSerializer.class)
     @JsonDeserialize(using = LocalDateTimeDeserializer.class)
-    @Schema(description = "The date when the premium span was updated", example = "12/15/2021", required = false, accessMode = Schema.AccessMode.READ_WRITE)
+    @Schema(description = "The date when the premium span was updated", example = "12/15/2021", requiredMode = Schema.RequiredMode.NOT_REQUIRED, accessMode = Schema.AccessMode.READ_WRITE)
     private LocalDateTime updatedDate;
 
     /**
@@ -168,17 +176,19 @@ public class PremiumSpanDto {
         return "PremiumSpanDto{" +
                 "premiumSpanSK=" + premiumSpanSK +
                 ", enrollmentSpanSK=" + enrollmentSpanSK +
-                ", premiumSpanCode='" + premiumSpanCode + '\'' +
                 ", ztcn='" + ztcn + '\'' +
+                ", premiumSpanCode='" + premiumSpanCode + '\'' +
                 ", memberPremiumSpans=" + memberPremiumSpans +
                 ", startDate=" + startDate +
                 ", endDate=" + endDate +
+                ", statusTypeCode='" + statusTypeCode + '\'' +
                 ", csrVariant='" + csrVariant + '\'' +
                 ", totalPremiumAmount=" + totalPremiumAmount +
                 ", totalResponsibleAmount=" + totalResponsibleAmount +
                 ", aptcAmount=" + aptcAmount +
                 ", otherPayAmount=" + otherPayAmount +
                 ", csrAmount=" + csrAmount +
+                ", changed=" + changed +
                 ", createdDate=" + createdDate +
                 ", updatedDate=" + updatedDate +
                 '}';
